8a90c7b5bfa645aa790c5b8f37cfe276daad891a
jiiro974 <jiiro@pm.me>
2026-03-19 16:48:56 +0400

    fix: correct v2.1.0 tag revision hash

--- Formula/clientshell.rb
+++ Formula/clientshell.rb
@@ -3,7 +3,7 @@ class Clientshell < Formula
   homepage "https://jiiro974.github.io/clientshell-releases"
   url "https://github.com/jiiro974/clientshell.git",
       tag:      "v2.1.0",
-      revision: "446c79621c3cac4a2e5ee6e80129d6d32e2cb7fa"
+      revision: "0ce9a7db79ffdbbc747c15c9d4b86be2ed2e6162"
   license "MIT"
   head "https://github.com/jiiro974/clientshell.git", branch: "main"
 
